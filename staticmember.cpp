39cd044621f11fef441d6a27525e5cfc9bba9005
tegartamburaka9-svg <tegartamburaka9@gmail.com>
2026-05-04 16:24:30 +0700

    membuat class mahasiswa

--- staticmember.cpp
+++ staticmember.cpp
@@ -18,32 +18,3 @@ public:
     }
 };
 
-//memberi value ke variabel nim dari class mahasiswa
-int Mahasiswa::nim = 10;
-
-//mengimplementasikan prosedur - prosedur di luar class
-void Mahasiswa::setID() {
-    id = ++nim;
-}
-
-void Mahasiswa::printAll() {
-    cout << "ID : " << id << endl;
-    cout << "Nama : " << nama << endl;
-    cout << endl;
-};
-
-int main() {
-    //membuat object dan memberi nilai
-    Mahasiswa mhs1("Lia Kurnia");
-    Mahasiswa mhs2("Asroni");
-    Mahasiswa mhs3("Andi Kurniawan");
-    Mahasiswa mhs4("Joko Purbo");
-
-    //memanggil prosedur printAll()
-    mhs1.printAll();
-    mhs2.printAll();
-    mhs3.printAll();
-    mhs4.printAll();
-
-    return 0;
-}
