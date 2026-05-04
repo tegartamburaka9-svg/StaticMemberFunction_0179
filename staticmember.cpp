cc189340d1f755f5098c82deea81c8ff138d4ab7
tegartamburaka9-svg <tegartamburaka9@gmail.com>
2026-05-04 16:26:49 +0700

    membuat setid

--- staticmember.cpp
+++ staticmember.cpp
@@ -21,3 +21,8 @@ public:
 //memberi value ke variabel nim dari class mahasiswa
 int Mahasiswa::nim = 10;
 
+//mengimplementasikan prosedur - prosedur di luar class
+void Mahasiswa::setID() {
+    id = ++nim;
+}
+
