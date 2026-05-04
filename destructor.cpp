88487142dc774f81fec0220d2e7ca58c9a32d476
tegartamburaka9-svg <tegartamburaka9@gmail.com>
2026-05-04 16:39:44 +0700

    void angka

--- destructor.cpp
+++ destructor.cpp
@@ -28,3 +28,9 @@ angka::~angka() { // Destructor
     cout << "Alamat Array Sudah Dilepaskan" << endl;
 }
 
+void angka::cetakData() {
+    for (int i = 1; i <= panjang; i++) {
+        cout << i << " = " << arr[i] << endl;
+    }
+}
+
